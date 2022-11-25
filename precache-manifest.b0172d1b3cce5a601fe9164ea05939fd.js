78496947a19326ec30e639baaa80de07b66b2d02
Fam Properties <75165054+fampropertiesgit@users.noreply.github.com>
2022-11-25 10:06:01 +0400

    Add files via upload

---
+++ precache-manifest.b0172d1b3cce5a601fe9164ea05939fd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f82f1f95446b9c20063c81a2e62a69a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b25d681a82340036c4",
+    "url": "/static/css/2.0977b175.chunk.css"
+  },
+  {
+    "revision": "84b25d681a82340036c4",
+    "url": "/static/js/2.fc12e15f.chunk.js"
+  },
+  {
+    "revision": "72ce9484b77e8ba15985",
+    "url": "/static/js/main.eacba0eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af0edc6d3278624eb46107564b39128e",
+    "url": "/static/media/blueprint-icons-16.af0edc6d.eot"
+  },
+  {
+    "revision": "d3fd53b07ff644d0d4f704d17dec1b81",
+    "url": "/static/media/blueprint-icons-16.d3fd53b0.woff"
+  },
+  {
+    "revision": "de7ba71d73b942e9a1333f818fbf01f8",
+    "url": "/static/media/blueprint-icons-16.de7ba71d.woff2"
+  },
+  {
+    "revision": "f03b034f09d1a1a2b6e3a3673a776ead",
+    "url": "/static/media/blueprint-icons-16.f03b034f.ttf"
+  },
+  {
+    "revision": "3715baa2895ceb6a290eaa75848d8f65",
+    "url": "/static/media/blueprint-icons-20.3715baa2.eot"
+  },
+  {
+    "revision": "899975dee116cb2cb82bf39fc99ca715",
+    "url": "/static/media/blueprint-icons-20.899975de.woff"
+  },
+  {
+    "revision": "9b1d21675a75cc5708e6da54db54d93d",
+    "url": "/static/media/blueprint-icons-20.9b1d2167.woff2"
+  },
+  {
+    "revision": "ba184ac3b34bc13d62dd1e79d43d7755",
+    "url": "/static/media/blueprint-icons-20.ba184ac3.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social/precache-manifest.beb75bf400a72fdb29822f5925cb12f9.js"
+  "/precache-manifest.b0172d1b3cce5a601fe9164ea05939fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
